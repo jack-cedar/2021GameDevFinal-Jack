e010c54f4f939b5c21f7d639ad32f8a53f23fe55
jack-cedar <78448269+jack-cedar@users.noreply.github.com>
2021-04-14 12:13:46 -0700

    commit

--- src/js/index.js
+++ src/js/index.js
@@ -9,10 +9,11 @@ function onAttack(){
     playerMain.attack(target);
 }
 
-let target = rat;
+
 let rat = new enemy(10, 2)
 let playerEntity = new entity (playerEntity)
 let playerMain = new player()
+let target = rat;
 ctx.fillRect(0, 0, 20, 20);
 
 
