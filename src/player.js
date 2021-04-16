9f9738c826c4fdb3c608ab0abf0f0ec79014c36d
jack-cedar <78448269+jack-cedar@users.noreply.github.com>
2021-04-16 09:50:31 -0700

    commit

--- src/player.js
+++ src/player.js
@@ -1,11 +1,8 @@
 export class player{
     constructor(){
-        this.lastPos = 
         this.position = [4,3];
         this.dimentions = [16, 16]
     }
-    
-    
     draw(ctx, tileX, tileY){
         ctx.fillRect(tileX*this.position[0], tileY*this.position[1], tileX ,tileY)
     }
