8e2d1ee79f30ed0f9caae772a8e759c871ca2aee
jack-cedar <jjanousekw@gmail.com>
2021-05-04 08:57:21 -0700

    commit

--- src/player.js
+++ src/player.js
@@ -36,8 +36,8 @@ export class player{
             case 0x00:this.CAN_MOVE = 1;break;
             case 0x10:this.CAN_MOVE = 1;break;
             case 0x0e:this.CAN_MOVE = 1;this.HAS_KEY = true; this.cMap[mapIndex(this.nPos[0], this.nPos[1])]=0x00;;break;
-            case 0x0f:this.CAN_MOVE = 1;if(this.HAS_KEY==true){this.CAN_MOVE = true;this.cMap[mapIndex(this.nPos[0], this.nPos[1])]=0x00};break;
-            default  :this.CAN_MOVE = 1; 
+            case 0x0f:this.CAN_MOVE = 0;if(this.HAS_KEY==true){this.CAN_MOVE = true;this.cMap[mapIndex(this.nPos[0], this.nPos[1])]=0x00};break;
+            default  :this.CAN_MOVE = 0; 
         }
         this.nPos[0] = this.cPos[0]
         this.nPos[1] = this.cPos[1]
