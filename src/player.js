be4bd433dde15f854b1dc4993d487f2fc0d7e5c0
jack-cedar <jjanousekw@gmail.com>
2021-05-04 09:49:57 -0700

    c

--- src/player.js
+++ src/player.js
@@ -36,7 +36,7 @@ export class player{
             case 0x00:this.CAN_MOVE = 1;break;
             case 0x10:this.CAN_MOVE = 1;break;
             case 0x0e:this.CAN_MOVE = 1;this.HAS_KEY = true; this.gameMap[mapIndex(this.nPos[0], this.nPos[1])]=0x00;;break;
-            case 0x0f:this.CAN_MOVE = 0;if(this.HAS_KEY==true){this.CAN_MOVE = true;this.gameMap[((this.cMap[mapIndex(this.cPos[0], this.cPos[1])]))]=0x00};break;
+            case 0x0f:this.CAN_MOVE = 0;if(this.HAS_KEY==true){this.CAN_MOVE = true;this.gameMap[((this.cMap[mapIndex(this.cPos[0], this.cPos[1])])*setTile)]=0x00};break;
             default  :this.CAN_MOVE = 0; 
         }
         this.nPos[0] = this.cPos[0]
