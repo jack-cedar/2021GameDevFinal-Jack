40d47f5f369762a31311de7c9a0464b1999e0894
jack-cedar <78448269+jack-cedar@users.noreply.github.com>
2021-05-20 14:15:32 -0700

    c

--- src/inputHandler.js
+++ src/inputHandler.js
@@ -6,10 +6,10 @@ export class input{
             
         if(player.IS_MOVING == true){return}
         switch (event.keyCode){
-        case 87:player.spriteFrame=1;player.moveU();break;
-        case 83:player.spriteFrame=0;player.moveD();break;
-        case 65:player.spriteFrame=2;player.moveL();break;
-        case 68:player.spriteFrame=3;player.moveR();break;}}
+        case 87:player.moveU();break;
+        case 83:player.moveD();break;
+        case 65:player.moveL();break;
+        case 68:player.moveR();break;}}
 
     }
 }
--- src/player.js
+++ src/player.js
@@ -1,5 +1,4 @@
 import { mapIndex } from "./index.js"
-
 export class player{
     constructor(xPos, yPos, speed, mapData, screenW, screenH){
         this.cPos = [xPos*8, yPos*8]
@@ -36,10 +35,10 @@ export class player{
                 this.nPos[1] = this.cPos[1]
             }
     }
-    moveU(){this.nPos[1]-=8;this.checkNpos();}
-    moveD(){this.nPos[1]+=8;this.checkNpos();}
-    moveL(){this.nPos[0]-=8;this.checkNpos();}
-    moveR(){this.nPos[0]+=8;this.checkNpos();}
+    moveD(){this.spriteFrame=0;this.nPos[1]+=8;this.checkNpos();}
+    moveU(){this.spriteFrame=1;this.nPos[1]-=8;this.checkNpos();}
+    moveL(){this.spriteFrame=2;this.nPos[0]-=8;this.checkNpos();}
+    moveR(){this.spriteFrame=3;this.nPos[0]+=8;this.checkNpos();}
     draw(ctx){ctx.drawImage(playerSprite, 16*this.spriteFrame,0,16,24, this.cPos[0], this.cPos[1],16,24);}
     update(){
         this.IS_MOVING=false
